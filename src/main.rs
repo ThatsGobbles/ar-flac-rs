043a6fa2f8255dc49b3d6a063181b567fe310e6f
Mark LeMoine <thatsgobbles@gmail.com>
2018-08-01 10:27:45 -0700

    Simple test of clap functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("ar-flac-rs")
+                    .version("1.0")
+                    .about("Validates FLAC files against the online AccurateRip database")
+                    .arg(
+                        Arg::with_name("FLAC_DIR")
+                        .help("path to directory of FLAC files to validate")
+                    )
+                    .get_matches();
+
+    println!("Using FLAC_DIR: {}", matches.value_of("FLAC_DIR").unwrap());
 }
